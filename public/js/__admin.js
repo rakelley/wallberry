1a1fa45598a4e411a685f4c94866f8d75ade4323
rakelley <ryanakelley@gmail.com>
2015-07-03 16:25:40 -0500

    Cleaned up admin js
    Changed js-only classes to data attributes and condensed duplicate functions

--- public/js/__admin.js
+++ public/js/__admin.js
@@ -3,36 +3,20 @@ var admin = {},
 
 
 admin.init = function() {
-    admin.adddirForm();
-    admin.deldirForm();
+    admin.backgroundForm('form[data-form_adddir]',
+                         'input[data-form_adddir-target]');
+    admin.backgroundForm('form[data-form_deldir]',
+                         'input[data-form_deldir-target]');
 };
 
-admin.adddirForm = function(formSelector, inputSelector) {
-    formSelector = formSelector || '.js-form_adddir';
-    inputSelector = inputSelector || 'input[data-form_adddir-target]';
+admin.backgroundForm = function(formSelector, inputSelector) {
+    var form = document.querySelector(formSelector),
+        input = form.querySelector(inputSelector);
 
-    var form = document.querySelector(formSelector);
     form.addEventListener('submit', function(e) {
         e.preventDefault();
 
-        var url = '/backgrounds/' + form.querySelector(inputSelector).value,
-            type = form.getAttribute('method');
-        utility.simpleAjax(url, type).then(
-            function() { location.reload(); },
-            function() { location.reload(); }
-        );
-    });
-};
-
-admin.deldirForm = function(formSelector, inputSelector) {
-    formSelector = formSelector || '.js-form_deldir';
-    inputSelector = inputSelector || 'input[data-form_deldir-target]';
-
-    var form = document.querySelector(formSelector);
-    form.addEventListener('submit', function(e) {
-        e.preventDefault();
-
-        var url = '/backgrounds/' + form.querySelector(inputSelector).value,
+        var url = '/backgrounds/' + input.value,
             type = form.getAttribute('method');
         utility.simpleAjax(url, type).then(
             function() { location.reload(); },
