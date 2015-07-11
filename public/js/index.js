34e78da531799abe34722f7f3930565427a192ff
rakelley <ryanakelley@gmail.com>
2015-07-11 13:02:59 -0500

    Recompiled for background url change

--- public/js/index.js
+++ public/js/index.js
@@ -1,16 +1,251 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-var index={},timeModule=require("./lib/datetime"),bgModule=require("./lib/background"),extModule=require("./lib/exterior"),intModule=require("./lib/interior");index.init=function(){timeModule(),bgModule();var e=extModule(),i=intModule();i?e.classList.remove("info_bar-item-large"):e.classList.add("info_bar-item-large")},index.init();
+var index = {},
+    timeModule = require('./lib/datetime'),
+    bgModule = require('./lib/background'),
+    extModule = require('./lib/exterior'),
+    intModule = require('./lib/interior');
+
+
+index.init = function() {
+    timeModule();
+
+    bgModule();
+
+    var exteriorTarget = extModule();
+
+    var interiorActive = intModule();
+
+    if (interiorActive) {
+        exteriorTarget.classList.remove('info_bar-item-large');
+    } else {
+        exteriorTarget.classList.add('info_bar-item-large');
+    }
+};
+
+index.init();
+
 },{"./lib/background":2,"./lib/datetime":3,"./lib/exterior":4,"./lib/interior":5}],2:[function(require,module,exports){
-var backgrounds={},utility=require("./utility");backgrounds.init=function(e,i,a){e=e||".bg-video",i=i||".bg-image",a=a||60,backgrounds.imageTarget=document.querySelector(i),backgrounds.videoTarget=document.querySelector(e),backgrounds.updateBackground(),window.setInterval(backgrounds.updateBackground,1e3*a)},backgrounds.updateBackground=function(){backgrounds.videoTarget.pause(),utility.simpleAjax("backgrounds","get").then(function(e){backgrounds.processFile(e)},function(){})},backgrounds.processFile=function(e){if(e&&void 0!=typeof e.file&&e.file){var i=e.file,a=backgrounds.getFileType(i);a&&("image"===a?backgrounds.updateImage(i):backgrounds.updateVideo(i,a))}},backgrounds.getFileType=function(e){var i=/(?:\.([^.]+))?$/,a=["mp4","webm","ogg"],o=["jpg","jpeg","png","gif"],t=i.exec(e)[1];return a.indexOf(t)>-1?"video/"+t:o.indexOf(t)>-1?"image":void 0},backgrounds.updateImage=function(e){backgrounds.imageTarget.src=e,utility.hide(backgrounds.videoTarget),utility.show(backgrounds.imageTarget)},backgrounds.updateVideo=function(e,i){backgrounds.videoTarget.children[0].setAttribute("src",e),backgrounds.videoTarget.children[0].setAttribute("type",i),backgrounds.videoTarget.load(),utility.hide(backgrounds.imageTarget),utility.show(backgrounds.videoTarget),backgrounds.videoTarget.play()},module.exports=backgrounds.init;
+var backgrounds = {},
+    utility = require('./utility');
+
+backgrounds.init = function(videoSelector, imageSelector, delay) {
+    videoSelector = videoSelector || ".bg-video";
+    imageSelector = imageSelector || ".bg-image";
+    delay = delay || 60;
+
+    backgrounds.imageTarget = document.querySelector(imageSelector);
+    backgrounds.videoTarget = document.querySelector(videoSelector);
+
+    backgrounds.updateBackground();
+    window.setInterval(backgrounds.updateBackground, delay * 1000);
+};
+
+backgrounds.updateBackground = function() {
+    backgrounds.videoTarget.pause();
+    utility.simpleAjax('random', 'get').then(
+        function(response) { backgrounds.processFile(response); },
+        function() { return; }
+    );
+};
+
+backgrounds.processFile = function(responseObject) {
+    if (!responseObject || typeof responseObject.file == undefined ||
+        !responseObject.file
+    ) {
+        return;
+    } else {
+        var file = responseObject.file;
+    }
+
+    var type = backgrounds.getFileType(file);
+    if (!type) {
+        return;
+    } else if (type === 'image') {
+        backgrounds.updateImage(file);
+    } else {
+        backgrounds.updateVideo(file, type);
+    }
+};
+
+backgrounds.getFileType = function(file) {
+    var regex = /(?:\.([^.]+))?$/,
+        videoTypes = ['mp4', 'webm', 'ogg'],
+        imageTypes = ['jpg', 'jpeg', 'png', 'gif'];
+
+    var type = regex.exec(file)[1];
+    if (videoTypes.indexOf(type) > -1) {
+        return 'video/' + type;
+    } else if (imageTypes.indexOf(type) > -1) {
+        return 'image';
+    } else {
+        return;
+    }
+};
+
+backgrounds.updateImage = function(file) {
+    backgrounds.imageTarget.src = file;
+    utility.hide(backgrounds.videoTarget);
+    utility.show(backgrounds.imageTarget);
+};
+
+backgrounds.updateVideo = function(file, type) {
+    backgrounds.videoTarget.children[0].setAttribute('src', file);
+    backgrounds.videoTarget.children[0].setAttribute('type', type);
+    backgrounds.videoTarget.load();
+    utility.hide(backgrounds.imageTarget);
+    utility.show(backgrounds.videoTarget);
+    backgrounds.videoTarget.play();
+};
+
+
+module.exports = backgrounds.init;
+
 },{"./utility":6}],3:[function(require,module,exports){
-var timeModule={};timeModule.init=function(e,t){e=e||".info_bar-clock",t=t||5;var i=document.querySelector(e);timeModule.updateTime(i),window.setInterval(timeModule.updateTime,1e3*t,i)},timeModule.updateTime=function(e){var t=new Date,i=timeModule.convertDay(t.getDay()),u=("0"+t.getHours()).slice(-2),o=("0"+t.getMinutes()).slice(-2);e.innerHTML=i+" "+u+":"+o},timeModule.convertDay=function(e){var t=["Sun","Mon","Tue","Wed","Thu","Fri","Sat"];return t[e]},module.exports=timeModule.init;
+var timeModule = {};
+
+timeModule.init = function(selector, delay) {
+    selector = selector || ".info_bar-clock";
+    delay = delay || 5;
+    var timeTarget = document.querySelector(selector);
+
+    timeModule.updateTime(timeTarget);
+    window.setInterval(timeModule.updateTime, delay * 1000, timeTarget);
+};
+
+timeModule.updateTime = function(target) {
+    var current = new Date(),
+        day = timeModule.convertDay(current.getDay()),
+        hours = ('0'+current.getHours()).slice(-2),
+        minutes = ('0'+current.getMinutes()).slice(-2);
+
+    target.innerHTML = day + ' ' + hours + ':' + minutes;
+};
+
+timeModule.convertDay = function(key) {
+    var days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+    return days[key];
+};
+
+
+module.exports = timeModule.init;
+
 },{}],4:[function(require,module,exports){
-var exterior={},utility=require("./utility");exterior.init=function(e,t){return e=e||".info_bar-w_exterior",t=t||1e3,exterior.target=document.querySelector(e),exterior.updateWeather(),window.setInterval(exterior.updateWeather,1e3*t),exterior.target},exterior.updateWeather=function(){utility.simpleAjax("exterior","get").then(function(e){exterior.processApi(e)},function(){})},exterior.processApi=function(e){if(e&&void 0!=typeof e.main&&e.main){var t=Math.round(e.main.temp),r=utility.escapeStr(e.weather[0].description);exterior.target.innerHTML=r+" "+t}},module.exports=exterior.init;
+var exterior = {},
+    utility = require('./utility');
+
+exterior.init = function(selector, delay) {
+    selector = selector || '.info_bar-w_exterior';
+    delay = delay || 1000;
+
+    exterior.target = document.querySelector(selector);
+
+    exterior.updateWeather();
+    window.setInterval(exterior.updateWeather, delay * 1000);
+
+    return exterior.target;
+};
+
+exterior.updateWeather = function() {
+    utility.simpleAjax('exterior', 'get').then(
+        function(response) { exterior.processApi(response); },
+        function() { return; }
+    );
+};
+
+exterior.processApi = function(responseObject) {
+    if (!responseObject || typeof responseObject.main == undefined ||
+        !responseObject.main
+    ) {
+        return;
+    } else {
+        var temp = Math.round(responseObject.main.temp),
+            description = utility.escapeStr(responseObject.weather[0].description);
+    }
+
+    exterior.target.innerHTML = description + ' ' + temp;
+};
+
+
+module.exports = exterior.init;
+
 },{"./utility":6}],5:[function(require,module,exports){
-var interior={},utility=require("./utility");interior.init=function(i,t){return i=i||".info_bar-w_interior",t=t||300,interior.target=document.querySelector(i),interior.updateTemperature(),window.setInterval(interior.updateWeather,1e3*t),interior.active},interior.active=!1,interior.updateTemperature=function(){utility.simpleAjax("interior","get").then(function(i){interior.processTemp(i)},function(){})},interior.processTemp=function(i){if(i&&void 0!=typeof i.temp&&i.temp){var t=Math.round(i.temp);interior.target.innerHTML=t,utility.show(interior.target),interior.active=!0}else interior.active=!1,utility.hide(interior.target)},module.exports=interior.init;
+var interior = {},
+    utility = require('./utility');
+
+interior.init = function(selector, delay) {
+    selector = selector || '.info_bar-w_interior';
+    delay = delay || 300;
+
+    interior.target = document.querySelector(selector);
+
+    interior.updateTemperature();
+    window.setInterval(interior.updateWeather, delay * 1000);
+    return interior.active;
+};
+
+interior.active = false;
+
+interior.updateTemperature = function() {
+    utility.simpleAjax('interior', 'get').then(
+        function(response) { interior.processTemp(response); },
+        function() { return; }
+    );
+};
+
+interior.processTemp = function(responseObject) {
+    if (!responseObject || typeof responseObject.temp == undefined ||
+        !responseObject.temp
+    ) {
+        interior.active = false;
+        utility.hide(interior.target);
+    } else {
+        var temp = Math.round(responseObject.temp);
+        interior.target.innerHTML = temp;
+        utility.show(interior.target);
+        interior.active = true;
+    }
+};
+
+
+module.exports = interior.init;
+
 },{"./utility":6}],6:[function(require,module,exports){
-var utility={};utility.simpleAjax=function(e,t){return new Promise(function(i,s){var n=new XMLHttpRequest;n.open(t,e,!0),n.onload=function(){200===n.status&&n.responseText?i(JSON.parse(n.responseText)):s()},n.send()})},utility.escapeStr=function(e){var t=document.createElement("div");return t.appendChild(document.createTextNode(e)),t.innerHTML},utility.hide=function(e){e.classList.remove("show"),e.classList.add("hide")},utility.show=function(e){e.classList.remove("hide"),e.classList.add("show")},module.exports=utility;
-},{}]},{},[1])
+var utility = {};
+
+utility.simpleAjax = function(url, type) {
+    return new Promise(function(resolve, reject) {
+        var req = new XMLHttpRequest();
+        req.open(type, url, true);
+
+        req.onload = function() {
+            if (req.status === 200 && req.responseText) {
+                resolve(JSON.parse(req.responseText));
+            } else {
+                reject();
+            }
+        }
+
+        req.send();
+    });
+};
+
+utility.escapeStr = function(str) {
+    var div = document.createElement('div');
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+};
+
+utility.hide = function(el) {
+    el.classList.remove('show');
+    el.classList.add('hide');
+};
+
+utility.show = function(el) {
+    el.classList.remove('hide');
+    el.classList.add('show');
+};
 
 
-//# sourceMappingURL=bundle.map
+module.exports = utility;
+
+},{}]},{},[1]);
